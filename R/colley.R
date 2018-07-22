833b54ef82e14a2da283cfe5498bb715c1d3730d
Scott Frechette <scottfrechette@gmail.com>
2018-07-22 09:21:03 -0500

    Create function to calculate Colley ratings

---
+++ R/colley.R
@@ -0,0 +1,61 @@
+calculate_colley <- function(schedule, scores) {
+
+  if(nrow(schedule) < n_distinct(schedule$Week) *
+     schedule %>%
+     select_at(vars(contains("Team"))) %>%
+     select(1) %>% n_distinct()) {
+
+    schedule <- schedule %>%
+      bind_rows(schedule %>%
+                  rename(Team1 = Team2, Team2 = Team1) %>%
+                  select(Week, Team1, Team2)) %>%
+      arrange(Week, Team1)
+  }
+
+  colley_df <- schedule %>%
+    left_join(scores, by = c("Week", "Team1" = "Team")) %>%
+    left_join(scores, by = c("Week", "Team2" = "Team")) %>%
+    mutate(x = if_else(Score.x > Score.y, "W",
+                       if_else(Score.y > Score.x, "L", "")) )%>%
+    group_by(Team1, Team2) %>%
+    summarise(x = paste(x, collapse = "")) %>%
+    ungroup() %>%
+    spread(Team2, x) %>%
+    mutate_if(is.character, replace_na, "X") %>%
+    unite(outcomes, -Team1, sep = "_") %>%
+    mutate(W = str_count(outcomes, "W"),
+           L = str_count(outcomes, "L"),
+           t = W + L,
+           Percent = percent(W/t),
+           Record = paste(W, L, sep = "-"),
+           C = t + 2,
+           b = 1 + (W - L) / 2)
+
+  colley_transform <- function(x) {
+    if(x == 0) {
+      0
+    } else if(x == "X") {
+      3
+    } else {
+      -nchar(x)
+    }
+  }
+
+  colley_invmat <- colley_df %>%
+    select(Team1, outcomes) %>%
+    separate(outcomes, cc$Team1) %>%
+    gather(Team, value, -Team1) %>%
+    mutate(x = map_dbl(value, colley_transform)) %>%
+    select(-value) %>%
+    spread(Team, x) %>%
+    select(-Team1) %>%
+    solve(tol = 1e-20)
+
+  colley_df %>%
+    mutate(Rating = colley_invmat %*% colley_df$b %>%
+             as.numeric() %>%
+             round(4),
+           Rank = min_rank(-Rating)) %>%
+    select(Team1, Record, Percent, Rating, Rank) %>%
+    arrange(Rank)
+}
