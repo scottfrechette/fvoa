2d5c17aa629d42118088f55ad5dfdb5ebaaa93b1
Scott Frechette <scottfrechette@gmail.com>
2018-08-23 23:12:18 -0500

    List more specific argument names

--- R/calculate_fvoa.R
+++ R/calculate_fvoa.R
@@ -1,12 +1,12 @@
 calculate_fvoa <- function(scores) {
 
-  all_matchups(df, type = "prob") %>%
+  all_matchups(scores, type = "prob") %>%
     select(-Team) %>%
     map_df(function(x) {round((mean(100 - x, na.rm = T) - 50) / 0.5, 2)}) %>%
     gather(Team, FVOA) %>%
     arrange(-FVOA) %>%
     mutate(`FVOA Rank` = dense_rank(-FVOA),
-           Week = max(df$Week))
+           Week = max(scores$Week))
 
 }
 
@@ -14,11 +14,11 @@ calculate_fvoa_season <- function(scores) {
 
   fvoa <- list()
 
-  for (i in 1:n_distinct(df$Week)) {
+  for (i in 1:n_distinct(scores$Week)) {
 
-    df_tmp <- df %>%
+    scores_tmp <- scores %>%
       filter(Week %in% 1:i)
-    fvoa_rankings <- calculate_fvoa(df_tmp)
+    fvoa_rankings <- calculate_fvoa(scores_tmp)
     fvoa[i] <- list(fvoa_rankings)
 
   }
