cb08ed5362992a17e5ee14cdfb1d54756d9c533a
Scott Frechette <scottfrechette@gmail.com>
2018-09-11 20:27:51 -0500

    Add way to manipulate all types of schedules

--- R/calculate_strength_schedule.R
+++ R/calculate_strength_schedule.R
@@ -1,8 +1,18 @@
 calculate_strength_schedule <- function(schedule, scores) {
 
+  if("Team" %in% names(schedule)) {
+    schedule <- spread_schedule(schedule)
+  }
+
+  schedule <- schedule %>%
+    mutate_if(is.factor, as.character)
+  schedule_rev <- schedule %>%
+    select(Week, Game_id, Team1 = Team2, Team2 = Team1)
+  schedule_both <- bind_rows(schedule, schedule_rev)
+
   calculate_fvoa(scores) %>%
     select(Team2 = Team, FVOA) %>%
-    right_join(schedule, by = "Team2") %>%
+    right_join(schedule_both, by = "Team2") %>%
     rename(Team = Team1) %>%
     group_by(Team) %>%
     summarise(SoS = round(mean(FVOA), 2)) %>%
