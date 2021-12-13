c3cd4c73f930e51f985b3bd2f1ab85f6656deb34
Scott Frechette <scottfrechette@gmail.com>
2021-12-13 12:17:56 -0600

    Add summarize_predicted_draws function

--- dev/test.R
+++ dev/test.R
@@ -1193,3 +1193,54 @@ fit %>%
 
 
 }
+
+
+# Functions ---------------------------------------------------------------
+
+scores %>%
+  add_predicted_draws(fit, value = "pred") %>%
+  summarize(mae = mean(abs(score - pred)),
+            l50 = quantile(pred, 0.25),
+            u50 = quantile(pred, 0.75),
+            l80 = quantile(pred, 0.1),
+            u80 = quantile(pred, 0.9),
+            l95 = quantile(pred, 0.025),
+            u95 = quantile(pred, 0.975)) %>%
+  mutate(within50 = between(score, l50, u50),
+         within80 = between(score, l80, u80),
+         within95 = between(score, l95, u95)) %>%
+  ungroup() %>%
+  mutate(sd = sd(score),
+         mae_scaled = mae / sd) %>%
+  summarize(mae = mean(mae),
+            mae_scaled = mean(mae_scaled),
+            within50 = mean(within50),
+            within80 = mean(within80),
+            within95 = mean(within95))
+
+summarize_predicted_draws <- function(predicted_draws,
+                                      truth = score,
+                                      prediction = pred) {
+
+  predicted_draws %>%
+    summarize(mae = mean(abs({{truth}} - {{prediction}})),
+              l50 = quantile({{prediction}}, 0.25),
+              u50 = quantile({{prediction}}, 0.75),
+              l80 = quantile({{prediction}}, 0.1),
+              u80 = quantile({{prediction}}, 0.9),
+              l95 = quantile({{prediction}}, 0.025),
+              u95 = quantile({{prediction}}, 0.975),
+              .groups = "keep") %>%
+    mutate(within50 = between({{truth}}, l50, u50),
+           within80 = between({{truth}}, l80, u80),
+           within95 = between({{truth}}, l95, u95)) %>%
+    ungroup() %>%
+    mutate(sd = sd({{truth}}),
+           mae_scaled = mae / sd) %>%
+    summarize(mae = mean(mae),
+              mae_scaled = mean(mae_scaled),
+              within50 = mean(within50),
+              within80 = mean(within80),
+              within95 = mean(within95))
+
+}
